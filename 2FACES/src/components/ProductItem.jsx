af6bd9e2988c59dc167a7989e099f3758239d05e
Mike Mutaki <Mkmutaki@gmail.com>
2025-03-02 02:31:50 -0800

    like funtionality 👍🏾

--- 2FACES/src/components/ProductItem.jsx
+++ 2FACES/src/components/ProductItem.jsx
@@ -1,15 +1,31 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai' 
 
 const ProductItem = ({id, image, name, price}) => {
 
     const {currency} = useContext(ShopContext);
 
+    const [like, setLike] = useState(true);
+    const likeNft = ()=> { if(!like){
+      setLike(true);
+    } else{
+      setLike(false);
+    }
+  }
+
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
       <div className='overflow-hidden'>
         <img className='hover:scale-110 tansition ease-in-out' src={image[0]} alt="" />
+        <div className='' onClick={() => likeNft()}>
+          {like ? (<AiOutlineHeart/>
+          ) : (
+            <AiFillHeart/>
+          )}
+          {""} 22
+        </div>
       </div>
       <p className='pt-3 pb-1 text-sm'>{name}</p>
       <p className='text-sm font-medium'>{currency}{price}</p>
