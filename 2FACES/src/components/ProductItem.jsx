99cd8dafe6374057431d596390ec4dcbb67134e3
Mike Mutaki <Mkmutaki@gmail.com>
2025-03-06 02:40:32 -0800

    like button not affecting product page

--- 2FACES/src/components/ProductItem.jsx
+++ 2FACES/src/components/ProductItem.jsx
@@ -16,20 +16,21 @@ const ProductItem = ({id, image, name, price}) => {
   }
 
   return (
-    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-      <div className='overflow-hidden'>
-        <img className='hover:scale-110 tansition ease-in-out' src={image[0]} alt="" />
-        <div className='' onClick={() => likeNft()}>
-          {like ? (<AiOutlineHeart/>
-          ) : (
-            <AiFillHeart/>
-          )}
-          {""} 22
+    <div className='product-item relative'>
+      <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+        <div className='relative'>
+          <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+          <div className='absolute bottom-0 left-0 flex flex-row gap-3 z-10 bg-white p-2' onClick={(event) => { event.stopPropagation(); event.preventDefault(); likeNft(); }}>
+            {like ? (<AiOutlineHeart/>
+            ) : (
+              <AiFillHeart/>
+            )}
+          </div>
         </div>
-      </div>
-      <p className='pt-3 pb-1 text-sm'>{name}</p>
-      <p className='text-sm font-medium'>{currency}{price}</p>
-    </Link>
+        <p className='pt-3 pb-1 text-sm'>{name}</p>
+        <p className='text-sm font-medium'>{currency}{price}</p>
+      </Link>
+    </div>
   )
 }
 
